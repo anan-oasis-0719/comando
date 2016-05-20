ebaab80e3687cf28181e4dff9a27c216fe914673
anan onda <anan.oasis.0719@gmail.com@gmail.com>
2016-05-20 23:34:23 +0900

    Initial Commit

---
+++ comando/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  comando
+//
+//  Created by onda anan on 2016/05/20.
+//  Copyright © 2016年 onda anan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
