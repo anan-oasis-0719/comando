60653aa0f51331e937caa6bef77fcf54d6ff8034
onda anan <anan.oasi.0719@gmail.com>
2016-05-21 22:39:44 +0900

    OH YEAH

--- comando/ViewController.swift
+++ comando/ViewController.swift
@@ -9,16 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var index:Int = 0
+    @IBOutlet var SayLabel :UILabel! //台詞を表示させるラベル
+    @IBOutlet var SayLabel2 :UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        SayLabel.text = "行っちまったのかと思ったよ。"
+        SayLabel.text = "とんでもねえ...待ってたんだ..."
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func next() {
+        
+        index = index + 1
+        
+        
+        
+    }
+
 
 
 }
